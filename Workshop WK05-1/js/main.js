144994e328208e4ee798ff13284fcf70c75d43f2
Jitpom <jittrapol@gmail.com>
2022-01-14 10:52:16 +0700

    addClass and removeClass

--- Workshop WK05-1/js/main.js
+++ Workshop WK05-1/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
     $("#heading-hero").html("DUST MITE SPRAY");
     $("#heading-hero").css("color", "red");
+    
+    //If the bars icon is clicked, 
+    //add the "active" class to the element with the id "top-nav"
+    $(".fa-bars").click(function(){
+        $("#top-nav").addClass("active");
+    });
+    
+    //If the times icon is clicked, 
+    //remove the "active" class from the element with the id "top-nav"
+    $(".fa-times").click(function(){
+        $("#top-nav").removeClass("active");
+    });
+
 });
